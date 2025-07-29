c6815d4dae1994dadf711a7f0ca2de3bc612f058
Shanat-Q <shanatpandey.5@gmail.com>
2025-07-29 13:50:52 +0530

    Initial commit for joblo AI

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/m99/", // <-- ✅ Add this line for GitHub Pages hosting
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
